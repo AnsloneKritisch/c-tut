653f713da2fd771c12c2f02c6256327545ff21c9
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-20 20:03:12 +0530

    nine

---
+++ maths_sums.c
@@ -0,0 +1,18 @@
+//WAP to find out the area of a circle ( use float values only )
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+
+{
+    float r = 4.5 , area ;
+    
+    area = 3.14 * r * r ;
+    
+    printf(" The area of the circle is = %f \n ",area ) ;
+
+    getch() ;
+
+    return 0;
+}
