d47a5a8cbef58a8f65ae4d5b75e8c4903e56a163
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-20 11:53:17 +0530

    five

--- operator.c
+++ operator.c
@@ -5,9 +5,15 @@
 
 int main()
 {
-    int a = 10 , b = 20 ,
-    c = a + b
-    d = b - b
-    
+    int a = 10 , b = 20 , c , d , e , f , g , h ;
+    c = a + b ;
+    d = b - a ;
+    e = a * b ;
+    f = b / a ;
+    g = b % a ;
+    h = ( a + b ) / 2 ;
+
+
+
     return 0;
 }
