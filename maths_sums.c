fd3c5196e934ffd2634e648a6af10c65d7ce4062
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-21 01:06:47 +0530

    ten

--- maths_sums.c
+++ maths_sums.c
@@ -13,30 +13,41 @@ These are mainly used with
 int main()
 
 {
-    float r = 4.5 , area ;
-    
-    area = 3.14 * r * r ;
-    
-    printf(" The area of the circle is = %f \n ",area ) ;
 
+//WAP to find area of circle 
+
+    float r = 4.5 , area1 ;
+    area1 = 3.14 * r * r ;
+    printf(" The area of the circle is = %f \n ",area1 ) ;
+
+// WAP to find area of Rectangle
+
+    int l = 10 , b = 20 , area ;
+    area = 2 * ( l + b ) ;
+    printf(" The area of the Rectangle is = %d \n ", area) ;
+    
     getch() ;
+
+
 
     return 0;
 }
 
-
+/*
 // WAP to find area of Rectangle
 
 #include <stdio.h>
 #include <conio.h>
 
-int main()
+int main1()
 
 {
     int l = 10 , b = 20 , area ;
     area = 2 * ( l + b ) ;
     printf(" The area of the Rectangle is = %d \n ", area) ;
     
-    getch()
+    getch() ;
+
     return 0;
 }
+ */
