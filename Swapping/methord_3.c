2d95f67254cd24cdc130b6a9eb57e7c862a5bce7
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-22 01:26:15 +0530

    16

--- Swapping/methord_3.c
+++ Swapping/methord_3.c
@@ -1,2 +1,17 @@
 // WAP to swap two numbers
-// METHORD 1
+// METHORD 3
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a = 10 , b = 20 ;
+    b = a+b - (a=b) ;
+    
+    printf( " The value of a = %d \n " , a) ;
+    printf( " The value of b = %d \n " , b) ;
+
+
+    return 0;
+}
