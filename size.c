0613a60c9adce42543356e7704687117de970840
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-22 01:48:54 +0530

    19

--- size.c
+++ size.c
@@ -1,1 +1,21 @@
-//WAP to find out how my space is taken by a integer 
+//WAP to find out how my space is taken by a integer , float and string .
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a = 1 ;
+    float b = 1.0 ;
+    char c = 'A'
+
+    printf(" The space occupied by integer = %d", sizeof(a)) ;
+    printf(" The space occupied by float = %f", sizeof(b)) ;
+    printf(" The space occupied by string = %s", sizeof(c)) ;
+
+    getch()
+    
+
+    return 0;
+}
+
