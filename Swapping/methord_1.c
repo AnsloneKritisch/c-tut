44413ca6e2b83a0be31bff75138eb721ea71b882
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-22 00:47:10 +0530

    14

---
+++ Swapping/methord_1.c
@@ -0,0 +1,18 @@
+// WAP to swap two numbers
+// METHORD 1
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int a = 1 , b = 2 , c ;
+    c = a ;
+    a = b ;
+    b = c ;
+    printf(" The value of b = %d \n " , b ) ;
+    printf(" The value of a = %d \n " , a ) ;
+    printf(" The value of c = %d \n " , c ) ;
+    
+
+    return 0;
+}
