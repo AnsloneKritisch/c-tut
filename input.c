5bc1023cb28a66588e3e8ecb3565257c094ef83c
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-23 21:46:19 +0530

    23

--- input.c
+++ input.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
 
+int main(int argc, char const *argv[])
+{
+    
+    int a , b ,c ;
+
+    printf(" Enter number a ") ;
+    scanf("%d" , &a ) ;
+
+    printf(" Enter number b ") ;
+    scanf("%d" , &b ) ;
+
+    c = a + b ;
+    printf( " The addision of two given no's are -> %d" , c ) ;
+    
+
+    
+    return 0;
+}
