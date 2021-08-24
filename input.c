5721a1ed5f5d5d1a0a1a5b54aee20cf0ba67b415
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-24 11:55:21 +0530

    26

--- input.c
+++ input.c
@@ -7,21 +7,21 @@ int main()
 {
     int a , b , c , d , e ;
 
-    printf(" Enter the big number = ") ;
+    printf(" Enter the big number = \n " ) ;
     scanf("%d" , &a ) ;
 
-    printf(" Enter the small number = ") ;
+    printf(" Enter the small number = \n ") ;
     scanf("%d" , &b ) ;
 
     c = a + b ;
-    printf( " The sum of two given no's are -> %d" , c ) ;
+    printf( " The sum of two given no's are -> %d \n " , c ) ;
 
     d = b - a ;
-    printf(" The difference between two no's are -> %d " , d ) ;
+    printf(" The difference between two no's are -> %d \n " , d ) ;
 
     e = b % a ;
-    printf(" The divide between two no is = " , e ) ;
-    
+    printf(" The divide between two no is = \n " , e ) ;
+
 
     getch() ;
             
