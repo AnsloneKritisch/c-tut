c302c3e177d8bf7abbccab8ecb6116c76743ee05
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-29 22:45:39 +0530

    32

---
+++ if-else/basic_format.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , b ;
+
+    printf("Write a number :");
+    scanf("%d" , &a ) ;
+    
+    printf("Write a number :");
+    scanf("%d" , &b ) ;
+
+    if ( a > b)
+    {
+        printf(" Yes a > b ")
+    }
+    
+
+    return 0;
+}
