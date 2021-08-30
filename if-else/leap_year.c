64fbffc8cf4db011489936fd1a781ed01ba8b336
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-31 00:08:43 +0530

    40

---
+++ if-else/leap_year.c
@@ -0,0 +1,23 @@
+// WAP to find a leap year or not 
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a ;
+
+    printf(" Write the year ") ;
+    scanf( "%d" , &a ) ;
+
+    if ( a % 4 == 0 && a % 100 != 0 ) || ( a % 400 == 0) 
+    {
+        printf(" It is a Leap Year ") ;
+    }
+    else
+    printf(" It is not a Leap Year ") ;
+
+    getch() ;
+       
+    return 0;
+}
