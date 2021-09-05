92d40070d64f5e67733b1487757b4db18c576c94
AnsloneKritisch <digveshoptimus@gmail.com>
2021-09-05 20:11:50 +0530

    42

---
+++ flow_of_control/for_loop/count.c
@@ -0,0 +1,21 @@
+// WAP to write a program for counting in range both accending and decending order .
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i , a ;
+
+    printf("Enter a range") ;
+    scanf("%d" , &a ) ;
+
+    for ( i = 1 ; i == a ; i++ ) ;
+    {
+        printf("%d" , i ) ;
+    }
+
+    getch() ;
+    
+    return 0;
+}
