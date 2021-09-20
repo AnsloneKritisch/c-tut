df2b8c1c39809aa7d36fd12c044a4332db6a4132
AnsloneKritisch <digveshoptimus@gmail.com>
2021-09-20 12:35:52 +0530

    71

---
+++ flow_of_control/for_loop/amstrong_no.c
@@ -0,0 +1,36 @@
+// WAP to check a number is Amstrong Number or not .
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , p , r , amg ;
+
+    printf("Enter the number :");
+    scanf("%d",&a);
+
+    p = a ;
+
+    for (;p>0;)
+    {
+        r = p%10 ;
+        amg = amg + (r*r*r) ;
+        p = p/10 ;
+    }
+
+    if (amg == a)
+    {
+        printf("Yes %d is an Amstrong number. ",a) ; 
+    }
+    else
+    {
+        printf("No %d is not an Amstrong number. ",a);
+    }
+
+
+    getch();
+       
+    return 0;
+    
+}
