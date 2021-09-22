621291cbd4efc3562877efa22fac9ca9c35159e2
AnsloneKritisch <digveshoptimus@gmail.com>
2021-09-22 18:46:24 +0530

    74

--- flow_of_control/for_loop/pattern1.c
+++ flow_of_control/for_loop/pattern1.c
@@ -20,9 +20,6 @@ int main()
         printf("\n");
         
     }
-
-    
-
     getch();
     
     return 0;
---
+++ flow_of_control/for_loop/pattern2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , i  ;
+
+    printf("Write the no : ");
+    scanf("%d",&a);
+
+    for ( i = 1 ; a <= i ; i--)
+    {
+        printf("%d \n ",i)
+    }
+    
+    
+    return 0;
+}
