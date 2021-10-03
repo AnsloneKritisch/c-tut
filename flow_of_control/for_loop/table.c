4ccfe62df7ade344aeef378f2bf1926bcfca5196
AnsloneKritisch <digveshoptimus@gmail.com>
2021-10-03 13:30:26 +0530

    95

---
+++ flow_of_control/for_loop/table.c
@@ -0,0 +1,30 @@
+// WAP to write a table .
+
+#include <stdio.h>
+#include <conio.h>
+ 
+int main() 
+{
+  int i , a , b , j ;
+
+  printf(" Write the number of tables you want : ") ;
+  scanf("%d" , &a ) ;
+
+  for ( i = 1 ; i <= a ; i++)
+  {
+      for ( j = 1 ; j <= 10 ; j++)
+      {
+          b = i*j ;
+
+          printf(" %d x %d = %d \n ", i , j , b ) ;
+
+      }
+      
+  }
+  
+
+  getch() ; 
+
+  return 0;
+    
+}
