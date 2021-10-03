c2e224b36ab77974ee5acda791116039839f12d0
AnsloneKritisch <digveshoptimus@gmail.com>
2021-10-03 12:06:34 +0530

    90

---
+++ flow_of_control/for_loop/pattern8.c
@@ -0,0 +1,29 @@
+//WAP to draw a pattern .
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , i , j , k ;
+
+    printf("Enter the number : ") ;
+    scanf("%d",&a) ;
+
+     for ( i = 1 ; i <= a ; i++)
+    {
+        for ( j = 1 ; j <= a - i ; j++) 
+        {
+            printf(" ") ;
+        }
+        for ( k=1 ; k <= 2*i-1 ; k++)
+        {
+            printf("*") ;
+        }
+        printf("\n") ;
+        
+    }
+
+    
+    return 0;
+}
