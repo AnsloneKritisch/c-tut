842095890ec1698e5c1d98ac23a113fd74fd8b2c
AnsloneKritisch <digveshoptimus@gmail.com>
2021-10-04 13:02:09 +0530

    100

---
+++ flow_of_control/for_loop/continue.c
@@ -0,0 +1,27 @@
+//WAP to use continue statment 
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , i ;
+    
+    printf("Enter a number : ") ;
+    scanf("%d",&a) ;
+
+    for ( i = 1 ; i <= a ; i++)
+    {
+        if (i == 5 )
+        {
+            continue ;
+        }
+
+        else printf("  %d \n", i ) ;
+        
+    }
+    getch();
+
+    return 0;
+    
+}
