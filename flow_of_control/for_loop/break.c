c1abf165a5be11a4f265b8c37f27184e1dcdc50b
AnsloneKritisch <digveshoptimus@gmail.com>
2021-10-04 12:54:38 +0530

    99

---
+++ flow_of_control/for_loop/break.c
@@ -0,0 +1,28 @@
+//WAP to use break and continue function.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , i ;
+
+    printf("Enter a number : ") ;
+    scanf("%d",&a) ;
+
+    for ( i = 1 ; i <= a ; i++)
+    {
+        if (i==5)
+        {
+            break ;
+        }
+
+        else printf(" %d \n ",i) ;
+        
+    }
+
+    getch() ;  
+    
+    return 0;
+
+}
