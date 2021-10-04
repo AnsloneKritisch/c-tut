6b271e19bb4188e704562265d8976e5b4956d654
AnsloneKritisch <digveshoptimus@gmail.com>
2021-10-04 20:51:32 +0530

    101

---
+++ flow_of_control/do_loop/factor.c
@@ -0,0 +1,26 @@
+//WAP a program to find factor using do-loop .
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , i ;
+
+    printf("Enter a number : ") ;
+    scanf("%d",&a) ;
+
+    i = 1 ;
+    while (i <= a )
+    {
+        printf("%d",i) ;
+
+        i++ ;
+
+    }
+
+    getch() ;  
+    
+    return 0;
+    
+}
