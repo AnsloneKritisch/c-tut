f4adcaa61daca8a68739d7d9828a5a049dab0a86
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-07 21:33:49 +0530

    Create amstrong_no.c

---
+++ flow_of_control/while_loop/amstrong_no.c
@@ -0,0 +1,17 @@
+// WAP to check a number is Amstrong Number or not .
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , p , r , amg = 0 ;
+
+    printf("Enter the number : ");
+    scanf("%d",&a);
+
+    
+
+    return 0;
+}
+
