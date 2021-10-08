dcb0a9a1b963e32d507d82c988ca0d57a9d45fca
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-08 20:52:19 +0530

    112

---
+++ flow_of_control/while_loop/perfect_no_range.c
@@ -0,0 +1,42 @@
+// Find all the perfect no. in the given range.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , b , i , j , c=0 ;
+
+    printf(" Write the Starting Range : ");
+    scanf("%d",&a);
+
+    printf(" Write the Ending Range : ");
+    scanf("%d",&b);
+
+    i = a ;
+    while (i <= b )
+    {
+        c = 0 ;
+
+        j = 1 ;
+        while (j <= i)
+        {
+            if (i%j==0)
+            {
+                c = c+j ;
+            }
+            j++ ;            
+        }
+
+        i++ ;    
+    }
+
+    if (c == i*2 )
+    {
+        printf("%d \n ", i );
+    }
+
+    getch();
+    
+    return 0;
+}
