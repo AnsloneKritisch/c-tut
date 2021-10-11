b3e45e222cda10db2f6a0a8289b2b4393775cc2f
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-11 20:52:22 +0530

    127

--- flow_of_control/for_loop/table.c
+++ flow_of_control/for_loop/table.c
@@ -5,20 +5,25 @@
  
 int main() 
 {
-  int i , a , b , j ;
+  int i , a , b , c , j ;
 
   printf(" Write the number of tables you want : ") ;
   scanf("%d" , &a ) ;
 
+  printf(" Write the number til which you want the table : ");
+  scanf("%d" , &c) ;
+
   for ( i = 1 ; i <= a ; i++)
   {
-      for ( j = 1 ; j <= 10 ; j++)
+      for ( j = 1 ; j <= c ; j++)
       {
           b = i*j ;
 
           printf(" %d x %d = %d \n ", i , j , b ) ;
 
       }
+
+      printf("\n");
       
   }
   getch() ; 
