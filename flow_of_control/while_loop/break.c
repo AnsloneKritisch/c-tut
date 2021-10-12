2d267942895d27a82be8af4252a9ae4a48b7d6ba
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-12 20:48:51 +0530

    136

--- flow_of_control/while_loop/break.c
+++ flow_of_control/while_loop/break.c
@@ -1,1 +1,28 @@
-//WAP 
+//WAP to use break function.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , i ;
+
+    printf("Enter a number : ") ;
+    scanf("%d",&a);
+
+    i = 1 ;
+    while (i <= a)
+    {
+        if (i == 5 )
+        {
+            break ;
+        }
+
+        else printf(" %d \n ", i );
+        
+        i++ ;
+    }
+    
+    
+    return 0;
+}
