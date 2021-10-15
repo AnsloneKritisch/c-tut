28325365ff096b8cf8cfef49f0b2ebf3f80efdf9
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-15 13:31:46 +0530

    148

---
+++ flow_of_control/while_loop/prime_no_range.c
@@ -0,0 +1,40 @@
+//WAP to find all prime no in the given range .
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , b , i , j , c=0 ;
+
+    printf(" Enter the Starting range : ");
+    scanf("%d",&a);
+
+    printf(" Enter the Ending range : ");
+    scanf("%d",&b);
+
+    i = a ;
+    while (i <= b )
+    {
+        c = 0 ;
+        
+        j = 1 ;
+        while (j <= i)
+        {
+            if (i%j == 0)
+            {
+                ++c;
+            }
+            if (c == 2)
+            {
+                printf("%d \n " , i ) ;
+            }
+            j++;
+        }
+        i++ ;
+    }
+
+    getch();
+    
+    return 0;
+}
