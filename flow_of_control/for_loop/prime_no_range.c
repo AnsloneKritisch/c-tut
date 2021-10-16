3037a18281f1ffa47d1dbc7b461f7c245918e9bc
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-16 10:13:42 +0530

    145

--- flow_of_control/for_loop/prime_no_range.c
+++ flow_of_control/for_loop/prime_no_range.c
@@ -15,20 +15,9 @@ int main()
 
     for ( i = a ; i <= b ; i++)
     {
-        c == 0 ;
-        for ( j = 1 ; j <= i ; j++ )
-        {
-            if ( i % j == 0 )
-            {
-                ++c ;
-            }
-        }
-        if (c == 2)
-        {
-            printf("%d" , i) ; 
-        }
-
+        
     }
+    
     
     getch() ;
 
