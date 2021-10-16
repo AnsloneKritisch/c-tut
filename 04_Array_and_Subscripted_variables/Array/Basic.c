5a1a153236542bdedc670fd613c539dbd4b05bbb
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-16 14:33:33 +0530

    154

---
+++ 04_Array_and_Subscripted_variables/Array/Basic.c
@@ -0,0 +1,27 @@
+// WAP to show basic program for array.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a[10] , no , i ;
+
+    printf("Enter how many no. : ");
+    scanf("%d",&no);
+
+    for ( i = 0; i < no ; i++)
+    {
+        printf("Enter the %d) number", i+1 ) ;
+        scanf("%d",&a[i]) ;
+    }
+    printf("The array is : ");
+    for ( i = 0; i < no ; i++)
+    {
+        printf("%d",a[i]);
+    }
+    
+    getch();
+       
+    return 0;
+}
