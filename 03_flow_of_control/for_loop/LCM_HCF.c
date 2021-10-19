c94778292c6d6a3883e752fbbe74d9b628ba91ca
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-19 21:35:28 +0530

    193

--- 03_flow_of_control/for_loop/LCM_HCF.c
+++ 03_flow_of_control/for_loop/LCM_HCF.c
@@ -1,1 +1,28 @@
-//WAP 
+//WAP to find LCM and HCF of a numbers.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , b , c , i , p , h , l ;
+
+    printf("Enter a number : ");
+    scanf("%d", &a );
+
+    printf("Enter another number : ");
+    scanf("%d",&b);
+
+    c = a+b ;
+
+    for ( i = 1; i <= c; i++)
+    {
+        if(a%i == 0 && b%i ==0)
+        {
+            printf("The HCF of %d and %d is = %d ",a,b,i);
+            break;
+        }
+    }
+        
+    return 0;
+}
