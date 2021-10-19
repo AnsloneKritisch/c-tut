6c301a1d0c2cc54f6a6854bb84c162f3a790f953
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-19 20:38:06 +0530

    186

---
+++ 04_Array_and_Subscripted_variables/factor.c
@@ -0,0 +1,36 @@
+//WAP to find the factor of all the number given input by the user.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , b[20] , c , i , j ;
+
+    printf("Total 20 number can be take input");
+
+    printf("Enter How many numbers you want to take input : ");
+    scanf("%d",&a);
+
+    for ( i = 1; i <= a; i++)
+    {
+        printf("Enter the %d) number : ", i );
+        scanf("%d",b[i]);
+    }
+
+    for ( i = 1; i <= a; i++)
+    {
+        printf("The factors of number %d are : ", b[i] )
+        for ( j = 1; j <= b[i] ; j++)
+        {
+            if(b[i] % j == 0)
+            {
+                printf("%d",j);
+            }
+        }   
+    }
+    
+    
+    
+    return 0;
+}
