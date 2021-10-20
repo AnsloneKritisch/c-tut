472f7628cc4d1b58f877f6a3886194271b09d7f8
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-20 13:45:18 +0530

    194

--- 03_flow_of_control/while_loop/LCM_HCF.c
+++ 03_flow_of_control/while_loop/LCM_HCF.c
@@ -1,1 +1,33 @@
 //WAP to find LCM and HCF of a number using While-loop.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a , b , c , i , product , hcf , lcm ;
+
+    printf("Enter a number : ");
+    scanf("%d",&a);
+
+    printf("Enter another number : ");
+    scanf("%d",&b);
+
+    c = a+b ;
+
+    i = 2 ;
+    while (i<=c)
+    {
+        if (a%i==0 && b%i==0)
+        {
+            printf("The HCF of %d and %d is = %d ",a,b,i);
+            hcf = i ;
+            break ;
+        }
+        
+        i++;
+    }
+    
+    
+    return 0;
+}
