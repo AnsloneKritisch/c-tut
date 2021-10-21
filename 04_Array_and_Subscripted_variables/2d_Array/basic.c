c5f3cbdc6c81f78fbec734190e0a614b1a45afbc
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-21 21:44:27 +0530

    203

---
+++ 04_Array_and_Subscripted_variables/2d_Array/basic.c
@@ -0,0 +1,42 @@
+// Matrix form is mainly a 2d Array.
+//WAP to take input a 2d array and print it out.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int ro , co , a[20][20] , i , j ;
+
+    printf("Enter number of rows : ");
+    scanf("%d",&ro);
+    
+    printf("Enter number of column : ");
+    scanf("%d",&co);
+
+    for ( i = 0; i < ro; i++)
+    {
+        for ( j = 0; j < co; j++)
+        {
+            printf("Enter the %d row and %d column : ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+        
+    }
+
+    printf(" The Matrix fo");
+
+    for ( i = 0; i < ro; i++)
+    {
+        for ( j = 0; j < co; j++)
+        {
+            printf("Enter the %d row and %d column : ",i,j);
+        }
+        
+    }
+
+    
+    
+    
+    return 0;
+}
