aeb41684951c896e87695b850feadf3339f9fca6
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-23 14:20:47 +0530

    204

--- try.c
+++ try.c
@@ -1,39 +1,41 @@
-//WAP to find the smallest number among the number input by the user.
-
-#include <stdio.h>
-#include <conio.h>
-
+#include<stdio.h>
+#include<conio.h>
 int main()
 {
-    int a , b[20] , i , j , sml ;
-
-    printf("Enter How many numbers : ");
-    scanf("%d",&a);
-
-    for ( i = 1; i <= a; i++)
-    {
-        printf("Enter %d) Number : ", i );        
-        scanf("%d",&b[i]);
-    }
-
-
-    for ( i = 1 ; i <= a ; i++)
-    {
-        if ( b[i] > b[i+1] )
-        {
-            b[i] = b[i+1] ;
-        }
-    }
-
-    printf("Arrange in assending order : ");
-
-     for ( i = 1; i <= a; i++)
-    {
-        printf(" %d ;", b[i] );        
-    }
-    
-
-    getch();
-
-    return 0;
+int a[10][10],ro,co,i,j,ldsum=0,rdsum=0;
+printf("Enter The Number Of Rows:");
+scanf("%d",&ro);
+printf("Enter The Number Of Columns:");
+scanf("%d",&co);
+printf("Enter The Numbers:");
+for(i=0;i<ro;i++)
+{
+for(j=0;j<co;j++)
+{
+scanf("%d",&a[i][j]);
+}
+}
+printf("The Numbers:\n");
+for(i=0;i<ro;i++)
+{
+for(j=0;j<co;j++)
+{
+printf("%d",a[i][j]);
+}
+printf("\n");
+}
+for(i=0;i<=ro;i++)
+{
+for(j=0;j<co;j++)
+{
+if(i==j)
+ldsum=ldsum+a[i][j];
+if(i+j==ro-1)
+rdsum=rdsum+a[i][j];
+}
+}
+printf("\nThe Left Diagonal Sum is=%d",ldsum);
+printf("\nThe Right Diagonal Sum is=%d",rdsum);
+getch();
+return 0;
 }
