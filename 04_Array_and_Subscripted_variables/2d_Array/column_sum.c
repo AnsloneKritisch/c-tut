c36dcb4774169e51db7c154d20438ef0c6223fa4
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-23 22:24:29 +0530

    224

---
+++ 04_Array_and_Subscripted_variables/2d_Array/column_sum.c
@@ -0,0 +1,64 @@
+//WAP to take input a Matrix and print all the prime number in the matrix.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int ro , co , a[20][20] , i , j , s ;
+
+    printf("Enter number of rows : ");
+    scanf("%d",&ro);
+    
+    printf("Enter number of column : ");
+    scanf("%d",&co);
+
+    printf(" \n ");
+    printf(" \n ");
+
+    for ( i = 0; i < ro; i++)
+    {
+        for ( j = 0; j < co; j++)
+        {
+            printf("Enter the %d row and %d column : ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+        
+    }
+
+    printf(" \n ");
+    printf(" \n ");
+
+    printf(" The Matrix we get is : \n ");
+
+    for ( i = 0; i < ro; i++)
+    {
+        for ( j = 0; j < co; j++)
+        {
+            printf(" %d ",a[i][j]);
+        }
+        printf(" \n ");
+        
+    }
+
+    printf(" \n ");
+    printf(" \n ");
+
+
+    for ( i = 0; i < co; i++)
+    {
+        for ( j = 0; j < ro; j++)
+        {
+            s = s + a[j][i] ;
+        }
+        printf(" The sum of all the numbers in column %d is = %d  \n ", i+1 , s ) ;
+        s = 0 ;        
+    }
+
+    printf(" \n ");
+    printf(" \n ");
+
+    getch();
+    
+    return 0;
+}
