32baa8641c35b2f41122ee98f73cffcfc8bb5ec0
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-24 22:02:37 +0530

    226

--- 04_Array_and_Subscripted_variables/2d_Array/Diagonal_Sum.c
+++ 04_Array_and_Subscripted_variables/2d_Array/Diagonal_Sum.c
@@ -17,3 +17,51 @@ int main()
             scanf("%d",&a[i][j]);
         }
     }
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a[10][10],ro,co,i,j,ldsum=0,rdsum=0;
+    printf("Enter The Number Of Rows:");
+    scanf("%d",&ro);
+    printf("Enter The Number Of Columns:");
+    scanf("%d",&co);
+    printf("Enter The Numbers:");
+    for(i=0;i<ro;i++)
+    {
+        for(j=0;j<co;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    
+    printf("The Numbers:\n");
+    for(i=0;i<ro;i++)
+    {
+        for(j=0;j<co;j++)
+        {
+            printf("%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<=ro;i++)
+    {
+        for(j=0;j<co;j++)
+        {
+            if(i==j)
+            ldsum=ldsum+a[i][j];
+            if(i+j==ro-1)
+            rdsum=rdsum+a[i][j];
+        }
+    }
+
+printf("\nThe Left Diagonal Sum is=%d",ldsum);
+
+printf("\nThe Right Diagonal Sum is=%d",rdsum);
+
+getch();
+
+return 0;
+
+}
