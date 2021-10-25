9866355df2150d10879e73dd9b3b5199b129dc99
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-25 14:15:50 +0530

    239

---
+++ 04_Array_and_Subscripted_variables/2d_Array/row_interchnage.c
@@ -0,0 +1,37 @@
+//WAP to interchange the row .
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int ro , co , a[20][20] , i , j ;
+
+    printf("Enter number of rows : ");
+    scanf("%d",&ro);
+    
+    printf("Enter number of column : ");
+    scanf("%d",&co);
+
+    for ( i = 0; i < ro; i++)
+    {
+        for ( j = 0; j < co; j++)
+        {
+            printf("Enter the %d row and %d column : ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+        
+    }
+
+    printf(" The Matrix we get is : \n ");
+
+    for ( i = 0; i < ro; i++)
+    {
+        for ( j = 0; j < co; j++)
+        {
+            printf(" %d ",a[i][j]);
+        }
+        printf(" \n ");
+        
+    }
+
