469e86c1c43b7a794104ad06150f26e90b019eb6
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-28 21:11:24 +0530

    250

---
+++ 05_String_Manipulation/basic.c
@@ -0,0 +1,17 @@
+//WAP to take input string and print it.
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st[90] ;
+
+    printf("Enter the string : ");
+    gets(st);
+
+    printf("%s",st);
+    
+    return 0;
+}
