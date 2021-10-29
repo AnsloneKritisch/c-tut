b58fb61b6b688e6276d3d858280c04eb083482ed
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-29 20:16:17 +0530

    257

--- 05_String_Manipulation/str_length_wo_fun.c
+++ 05_String_Manipulation/str_length_wo_fun.c
@@ -1,2 +1,22 @@
-//WAP to count the length of a string .
-//strlen -> is a function which calculates the length of string .
+//WAP to count the length of a string without using strlen function.
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st[90] ;
+    int i , a ;
+
+    printf("Enter a sentence : ");
+    gets(st);
+
+    for ( i = 0; st[i]!='\0'; ++i)
+    {
+        printf("%c \n " , st[i]) ;  
+    }
+    
+    
+    return 0;
+}
