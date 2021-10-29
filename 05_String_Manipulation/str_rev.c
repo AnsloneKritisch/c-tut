e72fd5c9d3c5f56806a6e3d20398a4aeb156b0ff
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-29 21:32:04 +0530

    263

---
+++ 05_String_Manipulation/str_rev.c
@@ -0,0 +1,23 @@
+//WAP to reverse a string .
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st[90] ;
+    int a ;
+
+    printf("Enter the sentence : ");
+    gets(st);
+
+    strrev(st);
+
+    printf("\n")
+    printf("Reversing the sentence : ");
+    printf(" %s ",st);
+
+
+    return 0;
+}
