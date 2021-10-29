c12cac91b2371e567e8e5409f962c257c81e8229
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-29 20:31:19 +0530

    258

---
+++ 05_String_Manipulation/slicing_str.c
@@ -0,0 +1,27 @@
+//WAP to print all the string one by one.
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st[90] ;
+    int i ;
+
+    printf("Enter a sentence : ");
+    gets(st);
+
+    printf("\n");
+    printf("\n");
+
+    printf("Printing the whole sentence in alphabet : \n");
+
+    for ( i = 0; st[i]!='\0'; ++i)
+    {
+        printf("%c \n " , st[i]) ;  
+    }
+    
+    
+    return 0;
+}
