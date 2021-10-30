b64c79e7ca4984edb541383c4db461b7be546aa2
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-30 23:51:03 +0530

    274

--- 05_String_Manipulation/str_cat.c
+++ 05_String_Manipulation/str_cat.c
@@ -6,13 +6,15 @@
 
 int main()
 {
-   char st1[90] , st2[90] ;
+   char st1[90] , st2[90] , st3[90] ;
     int i , a ;
 
     printf("\n");
 
     printf("Enter the 1st sentence : ");
     gets(st1);
+
+    strcpy(st3 , st1) ;
 
     printf("\n");
 
@@ -23,13 +25,15 @@ int main()
 
     printf("\n");
 
-    printf("After coping the 1st sentence into 2nd sentence : ") ;
+    printf("After Appending or Adding the 2nd sentence into 1st sentence : ") ;
     printf("%s",st1);
 
     printf("\n");
+    printf("\n");
     
+    strcat(st2 , st3 );
 
-    printf("After coping the 1st sentence into 2nd sentence : ") ;
+    printf("After  Appending or Adding the 1st sentence into 2nd sentence : ") ;
     printf("%s",st2);
 
     printf("\n");
