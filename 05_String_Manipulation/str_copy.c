893c9c476544881555d7129162e4ecfec1f865f1
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-30 10:24:55 +0530

    257

---
+++ 05_String_Manipulation/str_copy.c
@@ -0,0 +1,39 @@
+//WAP to take input string and copy it.
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st1[90] , st2[90] ;
+    int a ;
+
+    printf("\n");
+    printf("\n");
+
+    printf("Enter the 1st sentence : ");
+    gets(st1);
+
+    printf("\n");
+
+    printf("Enter the 2nd sentence : ");
+    scanf("%s", &st2);
+
+    printf("\n");
+
+    strcpy(st1,st2);
+
+    printf("%s", st2);
+    
+
+
+
+    
+    printf("\n");
+    printf("\n");
+
+    getch();
+
+    return 0;
+}
