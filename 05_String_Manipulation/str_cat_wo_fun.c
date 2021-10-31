9cfd64cbe8c279c5dc04457a8ce0b8a495874a2e
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-10-31 10:45:08 +0530

    270

---
+++ 05_String_Manipulation/str_cat_wo_fun.c
@@ -0,0 +1,52 @@
+//WAP to append or add one string on another without using strcat function. 
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+   char st1[90] , st2[90] , st3[90] ;
+    int i , a , b , c ;
+
+    printf("\n");
+
+    printf("Enter the 1st sentence : ");
+    gets(st1);
+
+    strcpy(st3 , st1) ;
+
+    printf("\n");
+
+    printf("Enter the 2nd sentence : ");
+    gets(st2);
+
+    a = strlen(st1);
+    b = strlen(st2);
+    c = a+b ; 
+
+    for ( i = a; i <= c ; i++)
+    {
+        st1[i] = st2[i];
+    }
+    
+
+    //printf("\n");
+
+    printf("After Appending or Adding the 2nd sentence into 1st sentence : ") ;
+    printf("%s",st1);
+
+    //printf("\n");
+    //printf("\n");
+    
+    /*strcat(st2 , st3 );
+
+    printf("After  Appending or Adding the 1st sentence into 2nd sentence : ") ;
+    printf("%s",st2);
+
+    printf("\n");
+*/    
+
+    getch();
+    return 0;
+}
