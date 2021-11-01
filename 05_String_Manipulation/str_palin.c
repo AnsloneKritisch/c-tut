5d629e833716cd433b5658e66a38d9f8fef32ec1
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-01 21:02:45 +0530

    271

--- 05_String_Manipulation/str_palin.c
+++ 05_String_Manipulation/str_palin.c
@@ -7,18 +7,20 @@
 int main()
 {
     char st1[100] , st2[100];
+    int i , a , c ;
 
     printf("Enter the word : ");
     gets("st1");
 
+    a = strlen(st1) ;
     strcpy(st2 , st1);
     strrev(st1);
 
-    for(i = 0; st[i]!='\0'; i++)
+    for(i = 0; i < a ; i++)
     {
         if (st2[i] == st1[i])
         {
-            printf("%s is a Palindrome");
+            c++;
         }
         else printf("%s is not a palindrome");
    
