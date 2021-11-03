0ecec2f74d4d41ba24a7fb56f3b48850c8f25b31
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-03 21:54:14 +0530

    290

--- 05_String_Manipulation/str_find_vowel.c
+++ 05_String_Manipulation/str_find_vowel.c
@@ -10,10 +10,13 @@ int main()
   printf("Input a string\n");
   gets(s);
 
-  while (s[c] != '\0') {
-    if (s[c] == 'a' || s[c] == 'A' || s[c] == 'e' || s[c] == 'E' || s[c] == 'i' || s[c] == 'I' || s[c] =='o' || s[c]=='O' || s[c] == 'u' || s[c] == 'U')
-      count++;
-    c++;
+  while (s[c] != '\0')
+   {
+       if (s[c] == 'a' || s[c] == 'A' || s[c] == 'e' || s[c] == 'E' || s[c] == 'i' || s[c] == 'I' || s[c] =='o' || s[c]=='O' || s[c] == 'u' || s[c] == 'U')
+       {
+           count++;
+           c++;
+       }
   }
 
   printf("Number of vowels in the string: %d", count);
