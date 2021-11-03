344f90c62befcf9c1f235a1689914ba5d01409c4
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-03 13:55:49 +0530

    282

---
+++ 05_String_Manipulation/count_word.c
@@ -0,0 +1,22 @@
+//WAP to count number of words in a sentence.
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st[100];
+    int i , c=0 ;
+
+    printf("Enter a sentence : ");
+    gets(st);
+
+    for ( i = 0; st[i]!='\0'; i++)
+    {
+
+    }
+    
+    
+    return 0;
+}
