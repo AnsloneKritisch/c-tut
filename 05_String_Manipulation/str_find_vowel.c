530ccad7037a73305215f1376a119d2e6d177157
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-03 23:33:13 +0530

    296

--- 05_String_Manipulation/str_find_vowel.c
+++ 05_String_Manipulation/str_find_vowel.c
@@ -7,35 +7,39 @@ int main()
     char line[100];
     int i , vowels=0, consonant=0 , digit=0 , space=0 ;
 
+    printf("Enter a sentence : ");
+    gets(line);
 
-  for (int i = 0; line[i] != '\0'; ++i)
-   {
-       if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
-       line[i] == 'o' || line[i] == 'u' || line[i] == 'A' ||
-       line[i] == 'E' || line[i] == 'I' || line[i] == 'O' ||
-       line[i] == 'U')
-       {
-           ++vowels;
-       }
-       else if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z'))
+    for (i = 0; line[i]!='\0'; i++)
+    {
+        if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+            line[i] == 'o' || line[i] == 'u' || line[i] == 'A' ||
+            line[i] == 'E' || line[i] == 'I' || line[i] == 'O' ||
+             line[i] == 'U')
+        {
+             ++vowels;
+        }
+        else if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z'))
         {
             ++consonant;
-        } 
+        }
         else if (line[i] >= '0' && line[i] <= '9')
         {
             ++digit;
-        } 
+        }
         else if (line[i] == ' ')
         {
             ++space;
-        }
+        } 
+        
+        
     }
 
     printf("Vowels: %d", vowels);
     printf("\nConsonants: %d", consonant);
     printf("\nDigits: %d", digit);
     printf("\nWhite spaces: %d", space);
-
-  return 0;
+    
+    return 0;
 }
 
