561d95dac41011696d21bfc196e01f431a3a5bd9
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-03 21:29:47 +0530

    286

--- 05_String_Manipulation/crypto.c
+++ 05_String_Manipulation/crypto.c
@@ -7,12 +7,32 @@
 int main()
 {
     char st[100] ;
-    int a ;
+    int i;
 
     printf(" Enter the sentence : ");
     gets(st);
 
+    for ( i = 0; st[i]!='\0'; i++)
+    {
+        if (st[i]>=65 && st[i]<90)
+        {
+            st[i]=st[i] + 1 ;
+        }
+        else if (st[i] == 90)
+        {
+            st[i] = 65 ;
+        }
+        else
+        {
+            printf("Something went wrong+")
+        }
+        
+        
+
+    }
     
+
+
 
     
     return 0;
