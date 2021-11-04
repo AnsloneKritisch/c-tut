9c346104f9edd4473247af2460a2ca48874bf6d7
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-04 23:54:23 +0530

    309

--- 05_String_Manipulation/word_gap.c
+++ 05_String_Manipulation/word_gap.c
@@ -10,4 +10,28 @@ output:
 
 */
 
-#include
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st[100];
+    int i ;
+
+    printf("\n");
+
+    printf("Enter a sentence : ");
+    gets(st);
+
+    for(i=0; st[i]!='\0'; i++)
+    {
+        if(st[i]==' ')
+        {
+            printf("\n");
+        }
+        else printf("%c",st[i]);
+    }
+    
+    return 0;
+}
