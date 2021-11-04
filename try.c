af78d79267466b6f2242fe93485de606808926d6
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-04 22:51:57 +0530

    300

---
+++ try.c
@@ -0,0 +1,31 @@
+//WAP to count number of words in a sentence.
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st[100];
+    int i , c=0 ;
+
+    printf("\n");
+
+    printf("Enter a sentence : ");
+    gets(st);
+
+    for ( i = 0; st[i]!='\0'; i++)
+    {
+        if (st[i] == ' ')
+        {
+            c++;
+        }
+    }
+    
+    printf("\n");
+    printf(" The Number of Sentences are = %d ",c+1);
+    printf("\n");
+    
+    getch();
+    return 0;
+}
