c20fc7bc0ce00a9828ee6118c129f73953505a3e
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-08 20:19:16 +0530

    245

--- 05_String_Manipulation/str_add_md_no.c
+++ 05_String_Manipulation/str_add_md_no.c
@@ -12,16 +12,22 @@ int main()
     char st[100];
     int i , a , c=0 , d=0 ;
 
+    printf("\n");
+
     printf(" Enter a sentence : ");
     gets(st);
-
+    
     a = strlen(st);
+    
+    printf(" After reversing the sentence we get : ");
 
     for ( i = a; i >= 0; i--)
     {
         printf("%c",st[i]) ;
     }
 
-    
+    printf("\n");
+
+    getch();
     return 0;
 }
