fdec2ae5dee01db3c728839911b3363f28405d00
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-08 18:17:02 +0530

    240

---
+++ 05_String_Manipulation/str_add_md_no.c
@@ -0,0 +1,14 @@
+//WAP to add all two or more digit number.
+/*
+Riya buy 20 oranges and 50 apples.
+*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    
+    return 0;
+}
