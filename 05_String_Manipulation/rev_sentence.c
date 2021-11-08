f2c5783bf7e7c03b80acc89acfb7b8d39bc37839
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-08 20:27:01 +0530

    248

--- 05_String_Manipulation/rev_sentence.c
+++ 05_String_Manipulation/rev_sentence.c
@@ -1,7 +1,3 @@
-//WAP to add all two or more digit number.
-/*
-Riya buy 20 oranges and 50 apples.
-*/
 
 #include <stdio.h>
 #include <conio.h>
---
+++ 05_String_Manipulation/str_add_md_no.c
@@ -0,0 +1,4 @@
+//WAP to add all two or more digit number.
+/*
+Riya buy 20 oranges and 50 apples.
+*/
