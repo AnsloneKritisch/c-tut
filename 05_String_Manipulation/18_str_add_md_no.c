d65c90c9e84a07592e05caa08360b79d39fc9671
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-12 20:55:34 +0530

    263

--- 05_String_Manipulation/18_str_add_md_no.c
+++ 05_String_Manipulation/18_str_add_md_no.c
@@ -7,6 +7,21 @@ Riya buy 20 oranges and 50 apples.
 #include <string.h>
 int main()
 {
+    int a , b ;
+    char st[100];
+
+    printf(" Enter the sentence : ");
+    gets(st);
+
+    for ( i = 0; st[i]!='/0'; i++)
+    {
+        if (st[i]==" " && st[i+1]>=48 && st[i+1]<=57)
+        {
+            printf(st[i+1]);
+        }
+        
+    }
+    
     
     
     return 0;
