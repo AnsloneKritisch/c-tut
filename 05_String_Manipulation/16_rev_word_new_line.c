96f72be60cc7947404462172f542119569df10eb
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-12 21:12:12 +0530

    265

---
+++ 05_String_Manipulation/16_rev_word_new_line.c
@@ -0,0 +1,49 @@
+//WAP to split each word into new line.
+/*
+Input : I am Anslone Kritisch
+
+output:
+ I
+ am
+ Anslone
+ Kritisch
+
+*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st[100];
+    int i ;
+
+    printf("\n");
+    
+    printf("Enter a sentence : ");
+    gets(st);
+
+    printf("\n");    
+
+    printf(" Input :- \n %s ",st) ;
+    strrev(st);
+
+    printf("\n");    
+
+    printf(" \n Output :- \n ") ;
+    for(i=0; st[i]!='\0'; i++)
+    {
+        if(st[i]==' ')
+        {
+            printf("\n");
+        }
+        else printf("%c",st[i]);
+    }
+
+    printf("\n");
+    printf("\n");
+    
+    getch();
+    return 0;
+}
