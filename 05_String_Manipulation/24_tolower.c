3c516972cefc81ff2d3d865e8232f0eaec5e2d94
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-12 23:36:13 +0530

    278

---
+++ 05_String_Manipulation/24_tolower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    char st = 'a' ;
+
+    tolower(st);
+
+    printf("%c",st);
+    
+    return 0;
+}
