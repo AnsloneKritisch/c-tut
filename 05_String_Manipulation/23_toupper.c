2080558d11c5d9f839c40faee82dc65d970caa48
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-13 12:58:09 +0530

    287

--- 05_String_Manipulation/23_toupper.c
+++ 05_String_Manipulation/23_toupper.c
@@ -1,12 +1,14 @@
 // C program to demonstrate
 // example of toupper() function.
-#include <ctype.h>
 #include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
 
 int main()
 {
 	char ch = 'a';
 	printf("%c in uppercase is represented as %c",ch,toupper(ch));
-
+	printf("\n");
+	getch();
 	return 0;
 }
