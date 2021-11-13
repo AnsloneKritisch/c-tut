54d1083a063a8f35bea9a61cc38d57136a909529
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-13 13:00:37 +0530

    289

--- 05_String_Manipulation/24_tolower.c
+++ 05_String_Manipulation/24_tolower.c
@@ -13,3 +13,17 @@ int main()
     
     return 0;
 }
+//WAP to illustrate toupper() function.
+
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+
+int main()
+{
+	char ch = 'a';
+	printf("%c in uppercase is represented as %c",ch,toupper(ch));
+	printf("\n");
+	getch();
+	return 0;
+}
