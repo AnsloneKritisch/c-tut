e3b2ce9d43e98fb6e49880656727b1e9f0ae83c4
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-14 12:06:25 +0530

    300

---
+++ 05_String_Manipulation/26_word_new_line_rev.c
@@ -0,0 +1,58 @@
+//WAP to split each word into new line.
+/*
+Input : I am Anslone Kritisch
+
+output:
+ I
+ am
+ Anslone
+ Kritisch
+
+*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st[100];
+    int i , j , a , b ;
+
+    printf("\n");
+    
+    printf("Enter a sentence : ");
+    gets(st);
+
+    printf("\n");    
+
+    printf(" Input :- \n %s ",st) ;
+
+    printf("\n");    
+
+    printf(" \n Output :- \n ") ;
+    for(i=0; st[i]!='\0'; i++)
+    {
+        if(st[i]==' ')
+        {
+            printf("\n");
+        }
+        else 
+        {
+            j = i ;
+            b = 1 ;
+            while (j<b)
+            {
+                printf("%s",st[j]);
+                j-- ;
+                b = j ;
+            }
+        }
+    }
+
+    printf("\n");
+    printf("\n");
+    
+    getch();
+    return 0;
+}
