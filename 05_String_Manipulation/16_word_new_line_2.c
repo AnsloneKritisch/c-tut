c5aa6849eebc5d2b35652a59c7f97d934f154683
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-19 11:20:16 +0530

    306

---
+++ 05_String_Manipulation/16_word_new_line_2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    char st[100] , pt[100] ;
+    int l , w= -1 ;
+
+    printf(" Enter a string : ");
+    gets(st) ;
+
+    for( l = 0; st[l]!='\0'; l++)
+    {
+        pt[l] = st[l];
+    }
+    pt[l] = 32 ;
+    pt[l+1] = '\0' ;
+
+    printf("%s",pt);
+
+    for( l = 0; pt[l]!='\0'; l++)
+    {
+        st[++w] = pt[l];
+        if (pt[l]==32)
+        {
+            st[w] = '\0';
+        }
+       
+        w = -1 ;
+    }
+    
+    
+    
+    return 0;
+}
