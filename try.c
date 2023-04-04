e2c9750482e521c0cc98ccdb88d70a4589d2f82e
AnsloneKritisch <digveshoptimus@gmail.com>
2023-04-04 15:45:49 +0530

    new

--- try.c
+++ try.c
@@ -1,9 +1,29 @@
+// strcat -> append one string at the end of another string
+
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>
+
+void add()
+{
+    a = a + b ;
+}
 
 int main()
 {
+    int a = 10 ;
+
+    && 
     
+    &
+
+    ||
+
+    !=
+
+    ==
+
+
 
     getch();    
     return 0;
